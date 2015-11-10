64850fab78359a54533cdbf8002935ca25300a2a
David Saenz Tagarro <david.saenz.tagarro@gmail.com>
2015-11-10 01:23:54 +0100

    Initial commit

---
+++ metadata.rb
@@ -0,0 +1,7 @@
+name             'apt'
+maintainer       'David Saenz Tagarro'
+maintainer_email 'david.saenz.tagarro@gmail.com'
+license          'All rights reserved'
+description      'Installs/Configures apt'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '0.1.0'
